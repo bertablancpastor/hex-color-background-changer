8095bd1fcaa19d30e4bc80fd65046faf96c94ae2
bertablancpastor <bertablanc89@gmail.com>
2023-10-19 11:03:02 +0200

    finalizado

--- js/app.js
+++ js/app.js
@@ -1,2 +1,41 @@
 // Implementar solución aquí
+// function changeHexColor() {
 
+// }
+
+function generarLetra() {
+  let letras = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
+
+  let numero = Math.floor(Math.random() * 16);
+  console.log(letras[numero]);
+  return letras[numero];
+}
+
+function colorHEX() {
+  //   console.log("Funciona?");
+  let coolor = "";
+  for (let i = 0; i < 6; i++) {
+    coolor = coolor + generarLetra();
+  }
+
+  document.querySelector("body").style.backgroundColor = "#" + coolor;
+
+  document.getElementById("hex-value").innerHTML = "#" + coolor;
+}
